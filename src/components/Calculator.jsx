37e36f4b68142b4cf56e5c986abe5c620e162e08
Anand Varkey Philips <anandvarkeyphilips@gmail.com>
2024-07-23 12:25:52 +0530

    Calculator using Uncontrolled Approach(createRef) with class components

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,22 +1,48 @@
-import { useRef } from "react"
+import { Component, createRef, useRef } from "react";
 
-export default function Calculator() {
+// export default function Calculator() {
 
-    let textBox1 = useRef(0);
-    let textBox2 = useRef(0);
+//     let textBox1 = useRef(0);
+//     let textBox2 = useRef(0);
 
-    const buttonHandler= () => {
-        let num1 = parseInt(textBox1.current.value);
-        let num2 = parseInt(textBox2.current.value);
-        alert("SUM is:"+(num1+num2));
-    }
+//     const buttonHandler= () => {
+//         let num1 = parseInt(textBox1.current.value);
+//         let num2 = parseInt(textBox2.current.value);
+//         alert("SUM is:"+(num1+num2));
+//     }
 
+//     return (
+//         <div>
+//             <h1>Calculator</h1>
+//             <input type="text" ref={textBox1} placeholder="Enter Number 1" />
+//             <input type="text" ref={textBox2} placeholder="Enter Number 2" />
+//             <button onClick={buttonHandler}>Add</button>
+//         </div>
+//     )
+// }
+
+export default class Calculator extends Component {
+  constructor() {
+    super();
+    this.textBox1 = createRef();
+    this.textBox2 = createRef();
+    this.buttonHandler = this.buttonHandler.bind(this);
+  }
+  
+  buttonHandler() {
+    let num1 = parseInt(this.textBox1.current.value);
+    let num2 = parseInt(this.textBox2.current.value);
+    alert("Sum is"+(num1+num2));
+  }
+
+  render() {
     return (
-        <div>
-            <h1>Calculator</h1>
-            <input type="text" ref={textBox1} placeholder="Enter Number 1" />
-            <input type="text" ref={textBox2} placeholder="Enter Number 2" />
-            <button onClick={buttonHandler}>Add</button>
-        </div>
-    )
+      <div>
+        <h1>Calculator</h1>
+        <input type="text" ref={this.textBox1} placeholder="Enter Number 1" />
+        <input type="text" ref={this.textBox2} placeholder="Enter Number 2" />
+        <button onClick={this.buttonHandler}>Add</button>
+      </div>
+    );
+  }
 }
